fc018e3d7ac269026b21c7379ddc86956956bb41
raywlfun <84743569+raywlfun@users.noreply.github.com>
2023-05-01 00:59:31 +0100

    Refactor webscraper to smaller functions, to make code more modular and improve testing. Add data cleaning function

--- Personal_Projects/Project_Webscraper/test_webscraper.py
+++ Personal_Projects/Project_Webscraper/test_webscraper.py
@@ -1,13 +1,31 @@
+from unittest.mock import patch
+
+import bs4
 import pytest
+import requests
 from Personal_Projects.Project_Webscraper.webscraper import (
     get_website_contents,
     create_sample_html_page,
     process_html_data,
 )
 
-# Remember to use 'assert' syntax
+TEST_HTML_DATA = '''
+    <html>
+        <head>
+            <title>Test HTML page</title>
+        </head>
+        <body>
+            <div class="Fabricated_statistics">
+                <p> This is some content </p>
+            </div>
+        </body>
+    </html>
+'''
 
-# TODO: Mock beautifulsoup with @patch, mock its return value, assert the result == expected result
+
+
+# TODO: Mock requests.get with @patch, mock its return value, assert the result == expected result
+@patch('requests.get')
 def test_get_website_content():
     assert get_website_contents('https://en.wikipedia.org/wiki/United_Kingdom')
 
--- Personal_Projects/Project_Webscraper/webscraper.py
+++ Personal_Projects/Project_Webscraper/webscraper.py
@@ -1,10 +1,14 @@
+import re
+
 from bs4 import BeautifulSoup
 from Personal_Projects.Project_Webscraper.sample_html import html_page_information, write_to_html_file
 import lxml
+import os
 import requests
 
 from dataclasses import dataclass
 import pandas as pd
+import html5lib
 
 
 # TODO: Encapsulate results of webscraped data to this dataclass
@@ -15,32 +19,64 @@ class ContinentData:
     mean_push_ups: int
 
 
-with open(write_to_html_file(html_page_information)) as file:
-    fabricated_html_data = file.read()
+# with open(write_to_html_file(html_page_information)) as file:
+#     fabricated_html_data = file.read()
+
+
+# Establish whether the input to be read is a filepath or url
+def read_html_content(html_url_or_filepath: str) -> str:
+    if os.path.isfile(html_url_or_filepath):
+        with open(html_url_or_filepath, 'r') as file:
+            html_content = file.read()
+            print('FILE READ')
+    else:
+        try:
+            web_response = requests.get(html_url_or_filepath, timeout=5)
+            html_content = web_response.content
+            print('URL READ')
+        except requests.exceptions.InvalidSchema:
+            print(f'Invalid URL @ {html_url_or_filepath}: failed to retrieve data')
+            return None
+    return html_content
+
+
+def parse_html_content(html_content: str) -> BeautifulSoup:
+    return BeautifulSoup(html_content, 'html.parser')
+
+
+def find_statistics_divs(page_content: BeautifulSoup) -> list:
+    return page_content.findAll('div', class_='Fabricated_statistics')
+
 
 
 def get_website_contents(html_url_or_filepath: str):
-    try:
-        web_response = requests.get(html_url_or_filepath, timeout=5)
-        page_content = BeautifulSoup(web_response.content, 'html.parser')
-    except requests.exceptions.InvalidSchema:
-        page_content = BeautifulSoup(html_url_or_filepath, 'html.parser')
-        print('No web response')
-    content_body = page_content.findAll('div', class_="Fabricated_statistics")
-    print(content_body)
-    return content_body
+    html_content = read_html_content(html_url_or_filepath)
+    if html_content is not None:
+        page_content = parse_html_content(html_content)
+        content_body = find_statistics_divs(page_content)
+        print(content_body)
+        return content_body
+    else:
+        return None
 
 
-get_website_contents(fabricated_html_data)
+raw_webscraped_data = get_website_contents('sample_page.html')
 
 
-def create_sample_html_page():
-    pass
+# Remove HTML tags from webscraped data
 
+def clean_webscraped_data(webscraped_data: str) -> list:
+    """
+    Personal learning note: Using re(.findall, .sub) to extract all numeric strings / replace all occurrences of a string by the string replacement
+    Personal research indicates this is a conventional way of removing html tags and unwanted formatting like commas, to acquire just the numerical data.
+    re = regular expression --> Regex
+    """
+    cleaned_data = []
+    numerical_strings = re.findall(r'\d+', webscraped_data)
+    for numerical_string in numerical_strings:
+        cleaned_numerical_string = re.sub(r'<.*?>', '', numerical_string)
+        cleaned_data.append(int(cleaned_numerical_string)) # Convert str to int
 
-def process_html_data():
-    pass
+    return cleaned_data
 
-
-def visualise_processed_statistics_data():
-    pass
+clean_webscraped_data(raw_webscraped_data)
