728444b6d2cfed4f5e3fb9a9a0b1579338bffef8
Ray Wong <raymond.wong@i4-insight.com>
2023-04-30 17:41:38 +0100

    Ignore; cloned project on new device and will continue there

--- Personal_Projects/Project_Webscraper/test_webscraper.py
+++ Personal_Projects/Project_Webscraper/test_webscraper.py
@@ -1,8 +1,25 @@
-from Project_Webscraper.webscraper import get_website_contents
+import pytest
+from Project_Webscraper.webscraper import (
+    get_website_contents,
+    create_sample_html_page,
+    process_html_data,
+)
 
 # Remember to use 'assert' syntax
+
+
 def test_get_website_content():
     assert get_website_contents('https://en.wikipedia.org/wiki/United_Kingdom')
 
 def test_fail_get_website_content():
     assert get_website_contents('sample_fail_test')
+
+
+def test_create_html_page():
+    pass
+
+
+# TODO: patch get website content function; add side effect and simulate 3x instances of different html data
+@pat
+def test_process_html_data():
+    pass
--- Personal_Projects/Project_Webscraper/webscraper.py
+++ Personal_Projects/Project_Webscraper/webscraper.py
@@ -2,6 +2,25 @@ from bs4 import BeautifulSoup
 import lxml
 import requests
 
+from dataclasses import dataclass
+import pandas as pd
+
+@dataclass
+class StatisticsData:
+    demographic_count: int
+    demographic_percentage: float
+    demographic_label: str
+
+
+    pass
+
+
+# @dataclass
+# class SampleHTML:
+#
+#     def __init__(self):
+
+
 def get_website_contents(website_url: str):
     web_response = requests.get(website_url, timeout=5)
     website_content = BeautifulSoup(web_response.content, 'html.parser')
@@ -9,3 +28,14 @@ def get_website_contents(website_url: str):
     print(content_body)
     return content_body
 
+
+def create_sample_html_page():
+    pass
+
+
+def process_html_data():
+    pass
+
+
+def visualise_processed_statistics_data():
+    pass
